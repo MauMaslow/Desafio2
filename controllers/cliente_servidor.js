1bdebfe442879143c24ec5122b9c2141179364c7
MauMaslow <105529342+MauMaslow@users.noreply.github.com>
2023-08-23 23:36:39 -0600

    Update cliente_servidor.js

--- controllers/cliente_servidor.js
+++ controllers/cliente_servidor.js
@@ -17,13 +17,17 @@ const crearproducto = (imgUrl,categorias,nombre,precio,descripcion) => {
         },
         //JSON.stringify tranforma el objeto lejible para JSON y agrega al contructor la libreria uuid para el id 
         body: JSON.stringify({imgUrl,categorias,nombre,precio,descripcion, id: uuid.v4()}),
-        });
+        })
+        .then(respuesta => respuesta.json())
+        .then(data => data.productos);
 };
 
 const eliminarproducto = (id) =>{    
     return fetch (`https://maumaslow.github.io/Desafio2/db.json/${id}`,{
     method: "DELETE"
-    });
+    })
+    .then(respuesta => respuesta.json())
+    .then(data => data.productos);
 };
 
 
@@ -33,6 +37,3 @@ export const productServices = {
     crearproducto,
     eliminarproducto,
 };
-
-
-
